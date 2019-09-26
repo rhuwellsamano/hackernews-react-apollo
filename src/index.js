1ccd0556ff5e864ec34a492f3fd406049a29d896
rhuwellsamano <rhuwell.samano@gmail.com>
2019-09-26 00:36:37 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+  })
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  })
+  
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
+  
+  serviceWorker.unregister();
